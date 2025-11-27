793046a5e4b0874eb926fb860bdf1a982d0ac450
Haveyou123 <92024748+Haveyou123@users.noreply.github.com>
2025-11-27 13:48:49 +0700

    Update

---
+++ Selenium/chromedriver-win32/chromedriver-win32/Selenium_BT5.py
@@ -0,0 +1,48 @@
+from pygments.formatters.html import webify
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+import re
+
+d = pd.DataFrame(columns={'Name': [], 'brith': [], 'death': [], 'nationality': []})
+
+driver = webdriver.Chrome()
+url = 'https://en.wikipedia.org/wiki/Edvard_Munch'
+driver.get(url)
+
+time.sleep(2)
+# Name
+try : 
+    name = driver.find_element(By.TAG_NAME, 'h1').text
+except:
+    name = ''
+# Brith
+try : 
+    brith_element = driver.find_element(By.XPATH, '//th[text()="Born"]/following-sibling::td')
+    brith = brith_element.text
+    brith = re.findall(r'[0-9]{1,2}+\s+[A-Za-z]+\s+[0-9]{4}', brith)[0]
+except:
+    brith = ''
+# Death
+try :
+    death_element = driver.find_element(By.XPATH, '//th[text()="Died"]/following-sibling::td')
+    death = death_element.text
+    death = re.findall(r'[0-9]{1,2}+\s+[A-Za-z]+\s+[0-9]{4}', death)[0]
+except:
+    death = ''
+# Nationality
+try:
+    nationality_element = driver.find_element(By.XPATH, '//th[text()="Nationality"]/following-sibling::td')
+    nationality = nationality_element.text
+except:
+    nationality = ''
+
+painter = {'Name': name, 'brith': brith, 'death': death, 'nationality': nationality}
+painter_df = pd.DataFrame([painter])
+d = pd.concat([d, painter_df], ignore_index=True)
+print(d)
+
+driver.quit()
+
+    
